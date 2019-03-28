3e56aca4986a0b87d045b996bec0c711803862e3
antheboets <anthe.boets@gmail.com>
2019-03-28 12:07:13 +0100

    adding sender/receiver for sales test

---
+++ receiverSales/reciver/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Xml;
+using Newtonsoft.Json;
+using System.IO;
+using System.Net;
+
+class ReceiveLogs
+{
+    public static void Main()
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.ExchangeDeclare(exchange: "logs", type: "fanout");
+
+            var queueName = channel.QueueDeclare().QueueName;
+            channel.QueueBind(queue: queueName,
+                              exchange: "logs",
+                              routingKey: "");
+
+            Console.WriteLine(" [*] Waiting for logs.");
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body;
+                var message = Encoding.UTF8.GetString(body);
+                
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml(message);
+                
+                string jsonText = JsonConvert.SerializeXmlNode(doc,Newtonsoft.Json.Formatting.None,true);
+                Console.WriteLine(jsonText);
+                //request naar elasticsearch
+                var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://10.3.56.26:9200/sales/_doc"); //url
+                httpWebRequest.ContentType = "application/json"; //ContentType
+                httpWebRequest.Method = "POST"; //Methode
+
+                //body
+                using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+                {
+                    streamWriter.Write(jsonText);
+                    streamWriter.Flush();
+                    streamWriter.Close();
+                }
+
+                var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse(); //sending request
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    String result = streamReader.ReadToEnd(); //get result Json string From respons
+                    Console.WriteLine("result");
+                    Console.WriteLine(result);
+                }
+
+            };
+            channel.BasicConsume(queue: queueName,autoAck: true,consumer: consumer);
+            Console.ReadLine();
+        }
+       
+    }
+}
---
+++ senderSales/sender/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+using System.Xml.Linq;
+class EmitLog
+{
+    public static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.ExchangeDeclare(exchange: "logs", type: "fanout");
+
+            String xmlStr = "<message> <header> <!-- type of message --> <MessageType>Purchase</MessageType> <!--What your Message does --> <description>Adds product purchase to user account</description> <!--Who sent it--> <sender/> <!-- kassa --> </header> <datastructure> <!-- required fields --> <UUID>001</UUID> <!-- id of the user --> <productname>pintje</productname> <price>1.00</price> <amount>2</amount> <total>2,00</total> <timestamp></timestamp> <version>1</version> <isActive>true</isActive> <!-- Not required fields --> <extraField/> </datastructure> </message> ";
+
+            var messagetest = XDocument.Parse(xmlStr);
+
+            var body = Encoding.UTF8.GetBytes(xmlStr);
+            channel.BasicPublish(exchange: "logs",routingKey: "",basicProperties: null,body: body);
+        }
+    }
+}
