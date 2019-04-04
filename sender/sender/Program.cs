7e3c360d78644c9c3f1a8d1bb4e7cbeed28f494d
antheboets <44194665+antheboets@users.noreply.github.com>
2019-04-04 12:19:31 +0200

    adding update and deletion xml to sender

--- sender/sender/Program.cs
+++ sender/sender/Program.cs
@@ -11,13 +11,31 @@ class EmitLog
         using (var channel = connection.CreateModel())
         {
             channel.ExchangeDeclare(exchange: "logs", type: "fanout");
+            String xmlStr;
+            byte[] body;
+            xmlStr = "<Message> <header> <!-- type of message --> <MessageType>Visitor</MessageType> <!--What your Message does --> <description>Creation of a visitor</description> <!--Who sent it--> <!--(fronted, crm, facturatie, kassa, monitor, planning, uuid) --> <sender>front-end</sender> <!-- kassa, crm, front-end --> </header> <datastructure> <!-- required fields = UUID name + email & hashing. --> <UUID>200</UUID> <!-- id of the user --> <name> <firstname>Anthe</firstname> <lastname>Boets</lastname> </name> <!-- kassa , front-end --> <email>anthe.boets@student.ehb.be</email> <GDPR>true</GDPR> <timestamp>1999-04-30T00:00:00+00:00</timestamp> <version>1</version> <isActive>true</isActive> <banned>false</banned> <!-- Not required fields --> <geboortedatum>1999-04-30T00:00:00+00:00</geboortedatum> <btw-nummer/> <gsm-nummer/> <extraField/> </datastructure> </Message>";
 
-            String xmlStr = "<Message> <header> <!-- type of message --> <MessageType>Visitor</MessageType> <!--What your Message does --> <description>Creation of a visitor</description> <!--Who sent it--> <!--(fronted, crm, facturatie, kassa, monitor, planning, uuid) --> <sender>front-end</sender> <!-- kassa, crm, front-end --> </header> <datastructure> <!-- required fields = UUID name + email & hashing. --> <UUID>100</UUID> <!-- id of the user --> <name> <firstname>Anthe</firstname> <lastname>Boets</lastname> </name> <!-- kassa , front-end --> <email>anthe.boets@student.ehb.be</email> <GDPR>true</GDPR> <timestamp>1999-04-30T00:00:00+00:00</timestamp> <version>1</version> <isActive>true</isActive> <banned>false</banned> <!-- Not required fields --> <geboortedatum>1999-04-30T00:00:00+00:00</geboortedatum> <btw-nummer/> <gsm-nummer/> <extraField/> </datastructure> </Message>";
-
-            var messagetest = XDocument.Parse(xmlStr);
-
-            var body = Encoding.UTF8.GetBytes(xmlStr);
+            body = Encoding.UTF8.GetBytes(xmlStr);
             channel.BasicPublish(exchange: "logs",routingKey: "",basicProperties: null,body: body);
+
+            Console.ReadKey();
+
+            channel.ExchangeDeclare(exchange: "logs", type: "fanout");
+
+            xmlStr = "<Message> <header> <!-- type of message --> <MessageType>Visitor</MessageType> <!--What your Message does --> <description>Update of a visitor</description> <!--Who sent it--> <!--(fronted, crm, facturatie, kassa, monitor, planning, uuid) --> <sender>front-end</sender> <!-- kassa, crm, front-end --> </header> <datastructure> <!-- required fields = UUID name + email & hashing. --> <UUID>200</UUID> <!-- id of the user --> <name> <firstname>Anthe234</firstname> <lastname>Boets</lastname> </name> <!-- kassa , front-end --> <email>anthe.boets@student.ehb.be</email> <GDPR>true</GDPR> <timestamp>1999-04-30T00:00:00+00:00</timestamp> <version>1</version> <isActive>true</isActive> <banned>false</banned> <!-- Not required fields --> <geboortedatum>1999-04-30T00:00:00+00:00</geboortedatum> <btw-nummer/> <gsm-nummer/> <extraField/> </datastructure> </Message>";
+
+            body = Encoding.UTF8.GetBytes(xmlStr);
+            channel.BasicPublish(exchange: "logs", routingKey: "", basicProperties: null, body: body);
+
+
+            Console.ReadKey();
+            channel.ExchangeDeclare(exchange: "logs", type: "fanout");
+
+            xmlStr = "<Message> <header> <!-- type of message --> <MessageType>Visitor</MessageType> <!--What your Message does --> <description>Deletion of a visitor</description> <!--Who sent it--> <!--(fronted, crm, facturatie, kassa, monitor, planning, uuid) --> <sender>front-end</sender> <!-- kassa, crm, front-end --> </header> <datastructure> <!-- required fields = UUID name + email & hashing. --> <UUID>200</UUID> <!-- id of the user --> <name> <firstname>Anthe234</firstname> <lastname>Boets</lastname> </name> <!-- kassa , front-end --> <email>anthe.boets@student.ehb.be</email> <GDPR>true</GDPR> <timestamp>1999-04-30T00:00:00+00:00</timestamp> <version>1</version> <isActive>true</isActive> <banned>false</banned> <!-- Not required fields --> <geboortedatum>1999-04-30T00:00:00+00:00</geboortedatum> <btw-nummer/> <gsm-nummer/> <extraField/> </datastructure> </Message>";
+
+            body = Encoding.UTF8.GetBytes(xmlStr);
+            channel.BasicPublish(exchange: "logs", routingKey: "", basicProperties: null, body: body);
+
         }
     }
 }
